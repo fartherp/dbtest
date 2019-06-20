707bfeaaec6fe3e0163eb715340f1faa06ec8fa5
fartherp <214722930@qq.com>
2019-06-20 14:42:53 +0800

    添加类注解单侧

--- dbtest-dbunit/src/test/java/com/github/fartherp/dbtest/dbunit/UserMapper.java
+++ dbtest-dbunit/src/test/java/com/github/fartherp/dbtest/dbunit/UserMapper.java
@@ -7,11 +7,15 @@ package com.github.fartherp.dbtest.dbunit;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 /**
  * `user`
  */
 @Mapper
 public interface UserMapper {
+
+	List<User> selectAll();
 
     User findByUsername(@Param("userName") String userName);
 }
